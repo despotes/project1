fb930c899fa30ab50d15f433a4e3560e81194636
despotes <pietromaddera@gmail.com>
2018-09-18 22:59:59 +0200

    removed unwanted files

--- prova.sql
+++
@@ -1,6 +0,0 @@
-SELECT title, author, year, isbn, COUNT(score), AVG(score)
-FROM books JOIN reviews ON book_id = books.id WHERE books.isbn = '1402319462'
-GROUP BY books.title;
-
-SELECT COUNT(score), AVG(score) FROM reviews JOIN books ON book_id=books.id
-WHERE books.isbn = '1402319462';
